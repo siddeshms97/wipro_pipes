afd8e74abc4c0cd0c6a462b4756ec6b15d7b4084
siddeshms97 <siddeshms18@gmail.com>
2021-10-12 14:09:37 +0530

    added logic

--- pipes.py
+++ pipes.py
@@ -1,1 +1,10 @@
+def pipes(arr,n):
+    temp = arr[0]
+    for i in range(1,n):
+        if arr[i] > temp:
+            print(arr[i],end = ' ', sep = '')
 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int,input().split()))
+    pipes(arr,n)
